fc11d1a718ebeec787b89f3f65089c6bf5d8a2e9
Ali Ibrahim <aliibra395@gmail.com>
2022-04-27 04:15:04 +0200

    Added login view|route

---
+++ src/views/Login/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-container>
+    <v-card class="mx-auto px-12 py-10 mt-6" max-width="600px">
+      <v-form
+          ref="form"
+          v-model="valid"
+          lazy-validation
+      >
+
+        <v-text-field
+            outlined
+            dense
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+        ></v-text-field>
+
+        <v-text-field
+            outlined
+            dense
+            v-model="password"
+            :rules="passwordRules"
+            label="Password"
+            type="password"
+            required
+        ></v-text-field>
+
+        <v-btn
+            :disabled="!valid"
+            color="primary"
+            class="mr-4"
+            @click="validate"
+        >
+          Login
+        </v-btn>
+
+        <v-btn
+            color="warning"
+            @click="resetValidation"
+        >
+          Reset Validation
+        </v-btn>
+      </v-form>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    ],
+    password: '',
+    passwordRules: [
+      v => !!v || 'Password is required'
+    ],
+  }),
+
+  methods: {
+    validate() {
+      this.$refs.form.validate()
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation()
+    },
+  },
+}
+</script>
